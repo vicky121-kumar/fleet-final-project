df51619dc68f6246098cf3b95a5d111149488d37
vicky121-kumar <vkkumarvicky001@gmail.com>
2020-07-17 09:38:58 +0530

    first commit

--- src/main/java/com/kumarvicky/demo/Controllers/LocationController.java
+++ src/main/java/com/kumarvicky/demo/Controllers/LocationController.java
@@ -1,14 +1,73 @@
 package com.kumarvicky.demo.Controllers;
 
+import com.kumarvicky.demo.models.Country;
+import com.kumarvicky.demo.models.Location;
+import com.kumarvicky.demo.models.State;
+import com.kumarvicky.demo.services.CountryService;
+import com.kumarvicky.demo.services.LocationService;
+import com.kumarvicky.demo.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class LocationController {
 
-    @GetMapping("location")
-    public String getLocation()
+    @Autowired
+    private CountryService countryService;
+    @Autowired
+    private StateService stateService;
+    @Autowired
+    private LocationService locationService;
+    //StateService locationService=new StateService();
+    @GetMapping("/location")
+    public String Home(Model model)
     {
-        return "Location";
+        List<State> stateList=stateService.getState();
+        List<Country> countryList=countryService.getCountries();
+        List<Location>locationList=locationService.getLocation();
+
+        model.addAttribute("state",stateList);
+        model.addAttribute("country",countryList);
+        model.addAttribute("location",locationList);
+        return "location";
     }
+    //Add new State
+    @PostMapping("/location/addNew")
+    public String addNew(Location location)
+    {
+        locationService.save(location);
+        return "redirect:/location";
+    }
+
+    //find location by id
+    @RequestMapping("location/findById")
+    @ResponseBody
+    public Optional<Location> findById(int id)
+    {
+        return locationService.findById(id);
+
+    }
+
+    // update location data
+    @RequestMapping(value = "/location/update",method = {RequestMethod.PUT,RequestMethod.GET})
+    public String updateLocation(Location location)
+    {
+        locationService.save(location);
+        return "redirect:/location";
+    }
+
+    //delete location
+    @RequestMapping(value = "/location/delete",method = {RequestMethod.DELETE,RequestMethod.GET})
+    public String deleteLocation(@RequestParam int id)
+    {
+        locationService.deleteLocation(id);
+        return "redirect:/location";
+    }
+
+
 }
--- src/main/java/com/kumarvicky/demo/services/LocationService.java
+++ src/main/java/com/kumarvicky/demo/services/LocationService.java
@@ -1,7 +1,39 @@
 package com.kumarvicky.demo.services;
 
+import com.kumarvicky.demo.models.Location;
+import com.kumarvicky.demo.models.State;
+import com.kumarvicky.demo.repositories.LocationRepository;
+import com.kumarvicky.demo.repositories.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LocationService {
+    @Autowired
+    private LocationRepository locationRepository;
+
+    //Return List of Location
+    public List<Location> getLocation()
+    {
+        return locationRepository.findAll();
+    }
+
+    //Save new Country
+    public void save(Location location){
+        locationRepository.save(location);
+    }
+
+    //get by id
+
+    public Optional<Location> findById(int id)
+    {
+        return locationRepository.findById(id);
+    }
+
+    public void deleteLocation(int id) {
+        locationRepository.deleteById(id);
+    }
 }
